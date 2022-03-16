239fe3a6480ad39251849ae17744e3b5df2b976f
gerencia@datalabcenter.com <gerencia@datalabcenter.com>
2022-03-16 02:33:55 -0500

    writing better comments

--- app.ts
+++ app.ts
@@ -1,15 +1,26 @@
+
+//import class Casher 
 import { Casher } from './utilities/Casher';
 
-console.clear();
-try {
-    const denominationList = [1, 5, 10];
-    const casherInstance: Casher = new Casher(denominationList);
-    const amount: number = 7;
+console.clear(); //I clear the console
 
+//Inside of this code block throw exceptions can ocurrs
+try {
+    const denominationList = [1, 5, 10]; //input array
+
+    //instance of Casher class
+    const casherInstance: Casher = new Casher(denominationList);
+
+
+    const amount: number = 7; //input amount
+
+    //result of the case
     const result: number = casherInstance.makeChange(amount);
 
+    //I print in console the result
     console.log(result);
 } catch (e) {
+    //if ocurrs an exception, I show an error log with red color
     console.error('\x1b[31m', e);
 }
 
--- utilities/Casher.ts
+++ utilities/Casher.ts
@@ -1,29 +1,37 @@
+//import my implementation of a sort method
 import { sortNumericArrays } from './sortUtilities';
+
+// I define the Cahser class for implement the business logic 
 class Casher {
+    //list of coins denominations
     _coinsDenominations: number[]
 
+    //constructor where the attributes are initialized
     constructor(pCoinsDenominations: number[]) {
 
+        //verify if all items of the array are integers
         const isAllCoinsIntegerValue = pCoinsDenominations.every((coin) => Number.isInteger(coin) && coin >= 0);
 
-        if (!isAllCoinsIntegerValue) {
+        if (!isAllCoinsIntegerValue) { //if array haven't a correct format, I throw an exception
             throw "All values of coins must be an integer value";
         }
 
+        //If the input array have a correct format, I initialize the attribute
         this._coinsDenominations = pCoinsDenominations
     }
 
+    //main method, answer the question of the exercise
     makeChange(pAmount: number): number {
         //this problems has the general cases and particular cases. I go to get first, the general case and resolve next the particular cases
 
-        let minCoins: number = this.getGeneralCase(pAmount); //get general cases
+        let minCoins: number = this.getGeneralCase(pAmount); //I get the result for the general cases
 
 
         //i go to work in the specific cases with a rotation matriz
-        let quantityOfRotation: number = 0;
-        let foundCombination: boolean = (minCoins < Number.MAX_SAFE_INTEGER) ? true : false;
+        let quantityOfRotation: number = 0; // I define the number of rotation that I have made
+        let foundCombination: boolean = (minCoins < Number.MAX_SAFE_INTEGER) ? true : false; // I define if I found a solution when review the general cases
 
-        while (quantityOfRotation < this._coinsDenominations.length) {
+        while (quantityOfRotation < this._coinsDenominations.length) { // while the rotations not finish, I try to find a solution
             let sum: number = 0;
             let quantity: number = 0;
             let partialValue: number = pAmount;
@@ -42,6 +50,7 @@ class Casher {
 
             }
 
+            //if the partial solution is the best, I change the value of the best solution
             const resultStep: number = (sum == pAmount) ? quantity : -1;
             if (resultStep > 0 && resultStep < minCoins) {
                 minCoins = resultStep;
@@ -53,10 +62,12 @@ class Casher {
 
         }
 
+        //I return the best solution or I notify that a solution doesn;t was founded
         return foundCombination ? minCoins : -1;
 
     }
 
+    //I implemented a solution for rotate the items of a matrix one position to the right
     private rotateArrayOnePositionRight(pInputArray: number[]): number[] {
         let resultArray: number[] = new Array<number>(pInputArray.length).fill(0);
         for (let i = 0; i < pInputArray.length; i++) {
@@ -69,6 +80,7 @@ class Casher {
         return resultArray;
     }
 
+    // I get a solution for the general cases 
     private getGeneralCase(pAmount: number): number {
         let sortedArray = [...this._coinsDenominations];
         sortNumericArrays(sortedArray, 'desc');
@@ -100,5 +112,5 @@ class Casher {
 
 
 
-
+// I export the class
 export { Casher }
